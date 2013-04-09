fb1bdc0d8b948c025c8084621e09470de97b81c5
sunra <sunra@yandex.ru>
2013-04-09 13:10:41 +0300

    5

--- src/HtmlToText/HtmlToText.php
+++ src/HtmlToText/HtmlToText.php
@@ -1,1 +1,25 @@
 <?php
+
+namespace Sunra\HtmlToText;
+
+
+class HtmlToText {
+	
+	/*
+	
+	
+	    $allowed_tags - br b strong ul li ol
+	
+	*/
+	
+	public static function convert( $html, $allowed_tags = '' ) {
+	
+	   $config = HTMLPurifier_Config::createDefault();
+       $purifier = new HTMLPurifier($config);
+       $clean_html = $purifier->purify($html);
+	   
+	   return $clean_html;
+	
+	}
+
+}
